2516ec647da8e66ef6eb5a403f4a59dada4f2578
linnnruoo <linruo2014@gmail.com>
2020-09-16 21:57:07 +0800

    modify App index.ts entry file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,45 @@
 import express, { Application } from 'express'
 import mongoose from 'mongoose'
 
-import indexRouter from './routes'
+import { MONGO_URI } from 'config/keys'
+import indexRouter from 'routes'
+import apiRouter from 'routes/api'
 
-const app: Application = express()
+class App {
+  public app: express.Application
+  public mongoUrl: string = MONGO_URI
 
-// Body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+  constructor() {
+    this.app = express()
+    this.setup()
+    this.mongoSetup()
+  }
 
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err: Error) => console.log(err))
+  private setup(): void {
+    // support application/json type post data
+    this.app.use(express.json())
+    // Support application/x-www-form-urlencoded post data
+    this.app.use(express.urlencoded({ extended: false }))
+    // Initialise http request to index and api routers '/' & '/api'
+    this.app.use('/', indexRouter)
+    this.app.use('/api', apiRouter)
+    // Listen to port 5000
+    this.app.listen('5000', () =>
+      console.log('Becky started listening on port 5000'),
+    )
+  }
 
-// Initialise http request to index router '/'
-app.use('/', indexRouter)
-
-app.listen('5000', () => console.log('Becky started listening on port 5000'))
+  private mongoSetup(): void {
+    try {
+      mongoose.connect(this.mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+export default new App().app

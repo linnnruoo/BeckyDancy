a895955bf296d680102c557eb7f4f42777daa1d5
linnnruoo <linruo2014@gmail.com>
2020-10-09 17:15:47 +0800

    update replica config to set default primary

--- mongo/replica/rs_config.js
+++ mongo/replica/rs_config.js
@@ -5,14 +5,17 @@ function initReplicaSet() {
       {
         _id: 0,
         host: 'mongo_1:27017',
+        priority: 1,
       },
       {
         _id: 1,
         host: 'mongo_2:27017',
+        priority: 0,
       },
       {
         _id: 2,
         host: 'mongo_3:27017',
+        priority: 0,
         arbiterOnly: true,
       },
     ],

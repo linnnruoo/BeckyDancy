7ab9571f002ae6a8678446e837d87f7d899365a6
linnnruoo <linruo2014@gmail.com>
2020-10-28 01:02:53 +0800

    modify user seeds

--- mongo/seeds/user.py
+++ mongo/seeds/user.py
@@ -2,9 +2,11 @@ from faker import Faker
 
 faker = Faker('en')
 
-POSSIBLE_USERS = ['Lynn', 'Dinesh', 'Tan', 'Tze Guang', 'Qi Kai', 'Ming Hong']
-POSSIBLE_URLS = ['https://i.imgur.com/WAm1CpL.jpg', 'https://i.imgur.com/jeqe4Al.png', 'https://i.imgur.com/rmCxK6Z.jpg',
-                 'https://i.imgur.com/dikGGpr.jpg', 'https://i.imgur.com/9wAAxYH.jpg', 'https://i.imgur.com/QBAkMuU.jpg']
+POSSIBLE_USERS = ['Lynn', 'Qi Kai', 'Ming Hong', 'Dinesh', 'Tan', 'Tze Guang']
+BEETLE_IDS = ['c8:df:84:fe:43:54', '2c:ab:33:cc:6c:ad',
+              '2c:ab:33:cc:5c:22', 'c8:df:84:fe:3f:df', '2c:ab:33:cc:6c:84', '2c:ab:33:cc:6a:92']
+POSSIBLE_URLS = ['https://i.imgur.com/WAm1CpL.jpg', 'https://i.imgur.com/9wAAxYH.jpg', 'https://i.imgur.com/QBAkMuU.jpg',
+                 'https://i.imgur.com/jeqe4Al.png', 'https://i.imgur.com/rmCxK6Z.jpg', 'https://i.imgur.com/dikGGpr.jpg']
 
 
 class UserSeeder:
@@ -22,7 +24,7 @@ class UserSeeder:
     def generate_user(self, i):
         return {
             'name': POSSIBLE_USERS[i],
-            'beetleId': str(i),
+            'beetleId': BEETLE_IDS[i],
             'MAC': faker.color(),
             'url': POSSIBLE_URLS[i]
         }
